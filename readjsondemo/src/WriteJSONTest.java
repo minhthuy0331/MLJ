f424f5285096d4e03387356cc0d8863aa16173ae
Minh Thúy <96002309+minhthuy0331@users.noreply.github.com>
2022-06-20 14:34:00 +0700

    Task4

---
+++ readjsondemo/src/WriteJSONTest.java
@@ -0,0 +1,44 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteJSONTest {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        // First Employee
+        JSONObject employeeDetails = new JSONObject();
+        employeeDetails.put("firstName", "Bui");
+        employeeDetails.put("lastName", "Thi Thuy");
+        employeeDetails.put("website", "codelean.vn");
+
+        JSONObject employeeObject = new JSONObject();
+        employeeObject.put("employee", employeeDetails);
+
+        // Second Employee
+        JSONObject employeeDetails2 = new JSONObject();
+        employeeDetails2.put("firstName", "Code");
+        employeeDetails2.put("lastName", "Lean");
+        employeeDetails2.put("website", "codelean.com");
+
+        JSONObject employeeObject2 = new JSONObject();
+        employeeObject.put("employee", employeeDetails2);
+
+        // Add employees to list
+        JSONArray employeeList = new JSONArray();
+        employeeList.add(employeeObject);
+        employeeList.add(employeeObject2);
+
+
+        // Write JSON file
+        try (FileWriter file = new FileWriter("employees.json")) {
+
+            file.write(employeeList.toJSONString());
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
